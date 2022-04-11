daa9dc5f0eca55e709be2df478fcf26adf9d52e5
rashedevs <rasheduap2015@gmail.com>
2022-04-12 05:02:34 +0600

    login-register component added with form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Login = () => {
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+        <form onSubmit={handleSubmit} style={{ margin: "30px" }}>
+            <input type="email" name="email" id="1" placeholder='Email' />
+            <br />
+            <input type="password" name="password" id="2" placeholder='Password' />
+            <br />
+            <button type="submit">Login</button>
+        </form>
+
     );
 };
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const Register = () => {
+    const handleRegSubmit = e => {
+        e.preventDefault()
+    }
     return (
-        <div>
-            <h2>Register</h2>
-        </div>
+
+        <form onSubmit={handleRegSubmit} style={{ margin: "30px" }}>
+            <input type="text" name="name" id="1" placeholder='Name' />
+            <br />
+            <input type="email" name="mail" id="2" placeholder='Email' />
+            <br />
+            <input type="password" name="pass" id="3" placeholder='Password' />
+            <br />
+            <button type="submit">Register</button>
+        </form>
+
     );
 };
 
