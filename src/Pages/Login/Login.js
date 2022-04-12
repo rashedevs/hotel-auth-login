83ace934d0958eccc0a6ebc2ae03d424571fd82e
rashedevs <rasheduap2015@gmail.com>
2022-04-12 23:59:19 +0600

    react-firebase-hook used for signup and login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import './Login.css'
 
 const Login = () => {
-    const handleSubmit = e => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmail = e => {
+        setEmail(e.target.value)
+    }
+    const handlePassword = e => {
+        setPassword(e.target.value)
+    }
+    if (user) { navigate('/') }
+
+    const handleUserLogin = e => {
         e.preventDefault()
+        signInWithEmailAndPassword(email, password)
     }
     return (
-        <form onSubmit={handleSubmit} style={{ margin: "30px" }}>
-            <input type="email" name="email" id="1" placeholder='Email' />
-            <br />
-            <input type="password" name="password" id="2" placeholder='Password' />
-            <br />
-            <button type="submit">Login</button>
-        </form>
+        <div className='form-container'>
+            <form onSubmit={handleUserLogin} style={{ margin: "30px" }}>
+                <input onBlur={handleEmail} type="email" name="email" id="1" placeholder='Email' required />
+                <br />
+                <input onBlur={handlePassword} type="password" name="password" id="2" placeholder='Password' required />
+                <br />
+                <p>{error?.message}</p>
+                <p>{loading && 'Loading...'}</p>
+                <br />
+                <input className='submit' type="submit" value="Login" />
+            </form>
+        </div>
 
     );
 };
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+
 
 const Register = () => {
-    const handleRegSubmit = e => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+    const handleName = e => {
+        setName(e.target.value)
+    }
+    const handleEmail = e => {
+        setEmail(e.target.value)
+    }
+    const handlePassword = e => {
+        setPassword(e.target.value)
+    }
+    if (user) {
+        navigate('/inventory')
+    }
+    if (name) { alert(name) }
+    const handleCreateUser = e => {
         e.preventDefault()
+
+        createUserWithEmailAndPassword(email, password)
     }
     return (
 
-        <form onSubmit={handleRegSubmit} style={{ margin: "30px" }}>
-            <input type="text" name="name" id="1" placeholder='Name' />
-            <br />
-            <input type="email" name="mail" id="2" placeholder='Email' />
-            <br />
-            <input type="password" name="pass" id="3" placeholder='Password' />
-            <br />
-            <button type="submit">Register</button>
-        </form>
+        <div className="form-container">
+            <form onSubmit={handleCreateUser} style={{ margin: "30px" }}>
+                <input onBlur={handleName} type="text" name="name" id="1" placeholder='Name' required />
+                <br />
+                <input onBlur={handleEmail} type="email" name="mail" id="2" placeholder='Email' required />
+                <br />
+                <input onBlur={handlePassword} type="password" name="pass" id="3" placeholder='Password' required />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
 
     );
 };
